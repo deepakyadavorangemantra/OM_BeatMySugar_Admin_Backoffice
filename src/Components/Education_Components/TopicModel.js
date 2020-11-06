7aeef500be502662331507c5714738ff6ba3d30c
deepak <93deep93@gmail.com>
2020-11-06 18:22:49 +0530

    Merge branch 'om_deepak' into main

--- src/Components/Education_Components/TopicModel.js
+++ src/Components/Education_Components/TopicModel.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-responsive-modal';
 import Notiflix from "notiflix";
+import CKEditor from 'ckeditor4-react';
 import { Flag } from 'react-feather';
 
 const ImgUpload =({
@@ -16,62 +17,56 @@ const ImgUpload =({
       id="photo-upload" type="file" onChange={onChange}/> 
     </label>
     
-const CreateChapterModel =(props)=>{
-    const [ chapterid, SetChapterid ] = useState('');
-    const [ isImageChange, SetImageChange ] = useState(false);
+const TopicModel =(props)=>{
+    const [  topicid, SetTopicid ] = useState('');
     const [ orderno, SetOrderno ] = useState(0);
     const [ Title , SetTitle ] = useState('');
     const [ Duration , SetDuration ] = useState('');
     const [ Description, SetDescription ] = useState('');
     const [ isActive, SetIsActive ] = useState('Yes');
     const [ AccreData, SetAccreData ] =useState([]);
-    const [ ImageData, SetImageData ] = useState([]);
+    // const [ ImageData, SetImageData ] = useState([]);
     const [ file, SetFile ] = useState('');
     const [ imagePreviewUrl, setImagePreviewUrl] = useState('https://www.adcproductdesign.com/wp-content/uploads/2018/02/Realize-Icon-Blue.png');
 
     useEffect( ()=>{
-        if(props.chapterEditData !== ''){
-            SetChapterid( props.chapterEditData ? props.chapterEditData.fld_chapterid : '');
-            SetTitle(props.chapterEditData ? props.chapterEditData.fld_title : '');
-            SetDuration(props.chapterEditData ? props.chapterEditData.fld_duration : '');
-            SetDescription(props.chapterEditData ? props.chapterEditData.fld_description : '');
-            SetIsActive(props.chapterEditData ? props.chapterEditData.fld_status === 'Active'? 'Yes':'No' : '');
-            SetOrderno(props.chapterEditData ? props.chapterEditData.fld_orderno : 0);
-            setImagePreviewUrl(props.chapterEditData ? props.chapterEditData.fld_bgimage : 'https://www.adcproductdesign.com/wp-content/uploads/2018/02/Realize-Icon-Blue.png');
-            SetImageData(props.chapterEditData ? props.chapterEditData.fld_bgimage : []);
+        if(props.topicEditData !== ''){
+            SetTopicid( props.topicEditData ? props.topicEditData.fld_id : '');
+            SetTitle(props.topicEditData ? props.topicEditData.fld_title : '');
+            
+            SetDescription(props.topicEditData ? props.topicEditData.fld_content : '');
+            SetIsActive(props.topicEditData ? props.topicEditData.fld_status === 'Active'? 'Yes':'No' : '');
+            SetOrderno(props.topicEditData ? props.topicEditData.fld_orderno : 0);
+            // setImagePreviewUrl(props.topicEditData ? props.topicEditData.fld_bgimage : 'https://www.adcproductdesign.com/wp-content/uploads/2018/02/Realize-Icon-Blue.png');
+            // SetImageData(props.topicEditData ? props.topicEditData.fld_bgimage : []);
         }
      },[])
 
-    function photoUpload (e){
-        e.preventDefault();
-        if (e.target.files[0].size < 100000) {
-        const reader = new FileReader();
-        const file = e.target.files[0];
-        reader.onloadend = () => {
-            SetFile(file);
-            setImagePreviewUrl(reader.result);
-            SetImageData(file);
-            SetImageChange(true);
-        }
-        reader.readAsDataURL(file);
-      } else {
-        Notiflix.Notify.Failure("File too large, upload file less than 100 kb.");
-      }
-      }
+    // function photoUpload (e){
+    //     e.preventDefault();
+    //     if (e.target.files[0].size < 100000) {
+    //     const reader = new FileReader();
+    //     const file = e.target.files[0];
+    //     reader.onloadend = () => {
+    //         SetFile(file);
+    //         setImagePreviewUrl(reader.result);
+    //         SetImageData(file);
+    //     }
+    //     reader.readAsDataURL(file);
+    //   } else {
+    //     Notiflix.Notify.Failure("File too large, upload file less than 100 kb.");
+    //   }
+    //   }
 
       function isValidate(){
         let flag= true;
-        if(JSON.stringify(ImageData) == '[]'){
-            flag = false;
-            Notiflix.Notify.Failure('Please upload thumbnail image.');
-        }
+        // if(JSON.stringify(ImageData) == '[]'){
+        //     flag = false;
+        //     Notiflix.Notify.Failure('Please upload thumbnail image.');
+        // }
         if(Title == ''){
             flag = false;
             Notiflix.Notify.Failure('Please enter chapter title.')
-        }
-        if(Duration == ''){
-            flag = false;
-            Notiflix.Notify.Failure('Please enter time duration of chapter.')
         }
         if(Description == ''){
             flag = false;
@@ -80,21 +75,20 @@ const CreateChapterModel =(props)=>{
         return flag;
       }
 
-      function saveChapter(){
+      function saveTopic(){
           if(isValidate()){
             let data ={}
-            data.ImageData = ImageData;
+            // data.ImageData = ImageData;
+            data.chapterid = props.select_chapter_id;
             data.title = Title;
             data.description = Description;
-            data.duration = Duration;
             data.orderno = orderno;
-            data.isImageChange = isImageChange;
             data.status = isActive == 'Yes' ? 'Active' : 'Inactive';
-            if(chapterid !=''){
-                data.id = chapterid;
-                props.updateChapterData(data);
+            if(topicid !=''){
+                data.id = topicid;
+                props.updateTopicData(data);
             }else{
-                props.saveChapterData(data);
+                props.saveTopicData(data);
             }
           }
       }
@@ -112,42 +106,54 @@ const CreateChapterModel =(props)=>{
 
                 <div className="modal-content modelcontent3">
                 <div className="modal-header">
-                    <h4 className="modal-title">Add New Chapter</h4>
+                    <h4 className="modal-title">Add Topic Chapter</h4>
                 </div>
                 <div className="modal-body">
-                        <div className="row">
-                        <div className="col-md-4">
-                        <div className="form-group mb-3">
-                            <label for="validationCustom01">Thumbnail Image<span className="mandatory">*</span></label>
-                        <div className="div1">
-                            <ImgUpload onChange={photoUpload} src={ imagePreviewUrl }/>
-                        
-                    </div>
-                        </div>
-                    </div>
-                    <div className="col-md-8">
+                    <div className="row">
+                        {/* <div className="col-md-4">
+                            <div className="form-group mb-3">
+                                <label for="validationCustom01">Thumbnail Image<span className="mandatory">*</span></label>
+                                <div className="div1">
+                                    <ImgUpload onChange={photoUpload} src={ imagePreviewUrl }/>
+                                
+                                </div>
+                            </div>
+                        </div> */}
+                    <div className="col-md-12">
                         <div className="row">
                             <div className="col-md-12" style={{    marginTop: '28px'}}>
+                                <div className="form-group mb-3">
+                                    <label for="validationCustom01">Chapter<span className="mandatory">*</span></label>
+                                        {/* <select id="region_id" name="select_chapter_id"  value={props.select_chapter_id} className="validate-select required-entry" defaultvalue="41" aria-required="true">
+                                            <option value="">Select Chapter</option> */}
+                                            { props.ChapterData.map( ( chapter, index) => {
+                                                // return <option value = {chapter.fld_chapterid}>{chapter.fld_title}</option>
+                                                if(chapter.fld_chapterid === props.select_chapter_id)
+                                                return <input type="text" className="form-control" 
+                                                value={chapter.fld_title}
+                                                disabled
+                                               />
+                                            })}
+                                        {/* </select> */}
+                                    </div>
+
                                 <div className="form-group mb-3">
                                     <label for="validationCustom01">Title<span className="mandatory">*</span></label>
                                     <input type="text" className="form-control" 
                                     value={Title}
                                     onChange={(e)=>{ SetTitle(e.target.value) }}/>
                                 </div>
-                                <div className="form-group mb-3">
-                                    <label for="validationCustom01">Duration<span className="mandatory">*</span></label>
-                                    <input type="time" className="form-control" 
-                                    value={Duration}
-                                    onChange={(e)=>{ SetDuration(e.target.value) }}/>
-                                </div>
+                                
                                 <div className="form-group mb-3">
                                     <label for="validationCustom01">Description<span className="mandatory">*</span></label>
                                    
-                                    <textarea type="text" class="form-control" 
-                                    rows="4" cols="10"
-                                    value={Description}
-                                    onChange={(e)=>{ SetDescription(e.target.value) }} >
-                                    </textarea>
+                                    <CKEditor
+                                            config={{
+                                            extraPlugins: "justify,font,colorbutton",
+                                            }}                                
+                                            data={Description}
+                                            onChange={(event)=>{  SetDescription(event.editor.getData())}}
+                                    />
                                 </div>
                             </div>
                         
@@ -178,7 +184,7 @@ const CreateChapterModel =(props)=>{
                 }}>Close</button>
                 
                 <button className="btn btn-primary" type="submit" style={{float:'right'}}
-                onClick={ saveChapter}>Save</button>
+                onClick={ saveTopic}>Save</button>
                     <span>
 
                     </span>
@@ -190,4 +196,4 @@ const CreateChapterModel =(props)=>{
     )
 }
 
-export default CreateChapterModel;
+export default TopicModel;
--- src/Pages/Education/Topics.js
+++ src/Pages/Education/Topics.js
@@ -1,20 +1,347 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet'
+import {Edit3,Trash2} from 'react-feather';
+import Modal from 'react-responsive-modal';
+import {connect} from 'react-redux';
+import Notiflix from "notiflix";
+import PostApiCall from '../../Api'
+import GetApiCall from '../../GetApi'
+import moment from 'moment';
+import { confirmAlert } from 'react-confirm-alert'; // 
+import TopicModel from '../../Components/Education_Components/TopicModel';
+import{
+    setSubCategoryName,
+    setSubOrder,
+    setClearArticleSubCategory
+}
+from '../../Components/Actions/ActionType';
 
 class EduTopics extends Component {
 
-    constructor(props) {
+    constructor(props){
         super(props)
         this.state = {
+           open:false,
+           select_chapter_id: '',
+           topicEditData :'',
+           TopicsList : [],
+           ChapterData :[],
+           Numregex : /^[0-9]*$/,
+           Status : 'Active',
+           Id : '',
+           openedit : false,
+         };
+       }
 
+    componentDidMount(){
+        Notiflix.Loading.Init({
+            svgColor : '#507dc0'    
+          });
+
+          Notiflix.Loading.Dots('Please wait...');
+          GetApiCall.getRequest("GetChapterMasterList").then(resultdes =>
+            resultdes.json().then(obj => {
+                if(obj.data.length>0){
+                    Notiflix.Loading.Dots('Please wait...');
+                    let default_chapter = obj.data[0];
+                    this.setState({ ChapterData : obj.data , select_chapter_id : default_chapter.fld_chapterid})
+                    this.handleGetTopicData(default_chapter.fld_chapterid);
+                   
+                }else{
+                    this.setState({    
+                        ChapterData : obj.data })
+                }
+               Notiflix.Loading.Remove()
+            }))
+
+        //   this.props.setClearArticleSubCategory()
+       
+      
         }
-    }
 
+        handleGetTopicData=( fld_chapterid)=>{
+            GetApiCall.getRequest("ListTopic?chapterid="+ fld_chapterid).then(resultdes =>
+                resultdes.json().then(obj => {
+                    this.setState({
+                        
+                    TopicsList : obj.data
+                  })
+                   Notiflix.Loading.Remove()
+                   console.log(obj.data)
+                }))
+        }
+        
+
+        handleChapterChange=(e)=>{
+            this.setState({[e.target.name] : e.target.value});
+            this.handleGetTopicData( e.target.value);
+        }  
+  
+
+        handleOrderChange = (order) =>{
+            if(this.state.Numregex.test(order.target.value)){
+            this.props.setSubOrder(order.target.value)
+            }
+        }
+
+        saveTopicData=( topicData)=>{
+            Notiflix.Loading.Dots('Please wait...');
+                var login=localStorage.getItem('LoginDetail');
+                    var details=JSON.parse(login)
+                    PostApiCall.postRequest ({
+
+                        chapterid : topicData.chapterid,
+                        title : topicData.title,
+                        content : topicData.description,
+                        orderno : topicData.orderno,
+                        img_url : '',
+                        type : '',
+                        createdon  : moment().format('lll'),
+                        createdby : details[0].fld_staffid,
+                        status : topicData.status
+                    },"AddTopic").then((resultTopic) =>
+                    resultTopic.json().then(objArticleSub => {
+                        if(resultTopic.status == 200 || resultTopic.status == 201){
+                            // this.props.setClearArticleSubCategory()
+                            Notiflix.Loading.Remove();
+                            Notiflix.Notify.Success('Topic successfully added.')
+                            window.location.reload()
+                        }else
+                        {
+                          Notiflix.Loading.Remove();
+                          Notiflix.Notify.Failure('Topic already present.')
+                        }
+                    })
+                )
+        }
+
+        updateTopicData=(topicData)=>{
+            Notiflix.Loading.Dots('Please wait...');
+            var login=localStorage.getItem('LoginDetail');
+                var details=JSON.parse(login)
+
+                PostApiCall.postRequest ({
+
+                     title : topicData.title,
+                     content: topicData.description,
+                     orderno: topicData.orderno,
+                     img_url :'',
+                     type :'',
+                     status : topicData.status,
+                     topicid : topicData.id,
+                    updatedby : details[0].fld_staffid,
+                    updatedon : moment().format('lll')
+                },"UpdateTopic").then((resultTopic) =>
+                resultTopic.json().then(objArticleSub => {
+                    if(resultTopic.status == 200 || resultTopic.status == 201){
+                        // this.props.setClearArticleSubCategory()
+                        Notiflix.Loading.Remove();
+                        Notiflix.Notify.Success('Topic successfully updated.')
+                        window.location.reload()
+                    }else
+                    {
+                        Notiflix.Loading.Remove();
+                        Notiflix.Notify.Failure('Topic already present.')
+                    }
+                })
+            )
+    
+        }
+         
     render(){
+        const { ChapterData, select_chapter_id }= this.state
         return(
-            <React.Fragment>
-                <h1> Topics </h1>
-            </React.Fragment>
+           <div>
+                    
+            <div className="content-page">
+            
+            <div className="content">   
+            { this.state.open === true?
+                <TopicModel
+                open={this.state.open} 
+                topicEditData = {this.state.topicEditData}
+                ChapterData= {this.state.ChapterData}
+                select_chapter_id={this.state.select_chapter_id}
+                closeModel={()=>{ this.setState({ open : false, chapterEditData:''})}}
+                saveTopicData={this.saveTopicData}
+                updateTopicData={this.updateTopicData}
+                />:''} 
+
+                <div className="container-fluid">
+                    <div className="row page-title">
+                        <div className="col-md-12">
+                            <nav aria-label="breadcrumb" className="float-right mt-1">
+                                <ol className="breadcrumb">
+                                    <li className="breadcrumb-item"><a href="#">Education Module</a></li>
+                                    <li className="breadcrumb-item active" aria-current="page">Chapter's Topics</li>
+                                </ol>
+                            </nav>
+                            <h4 className="mb-1 mt-0">Topic Master</h4>
+                        </div>
+                    </div> 
+
+                    <div className="row">
+                    <div className="col-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row align-items-center">
+                                    <div className="col-md-8 text-left">
+                                        {/* chapter list */}
+                                        <div className="field chapter required">
+                                            <label className="label"><span>Chapter</span></label>
+                                            <div className="control">
+                                                <select id="region_id" name="select_chapter_id"  onChange={this.handleChapterChange} value={select_chapter_id} className="validate-select required-entry" defaultvalue="41" aria-required="true">
+                                                    { ChapterData.map( ( chapter, index) => {
+                                                        return <option value = {chapter.fld_chapterid}>{chapter.fld_title}</option>
+                                                    })}
+                                                </select>
+                                                {/* {errors.country_id ?  <div  className = "error">{errors.country_id}</div> : '' } */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4 text-right">
+                                        <button 
+                                        onClick={()=>{
+                                            this.setState({
+                                                open : true
+                                            })
+                                        }}
+                                        className="btn btn-primary" id="btn-new-event" data-toggle="modal"><i
+                                                className="uil-plus mr-1"></i>Add New Topic</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                
+                </div>
+                    
+                    
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="card">
+                                <div className="card-body">
+                                <div className="table-responsive">
+                                <table id="basic-datatable" className="table dt-responsive nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Order</th>
+                                        <th>Status</th>
+                                        <th>Updated On</th>
+                                        <th>Action</th>
+                                       
+                                        
+                                    </tr>
+                                </thead>
+                            
+                            
+                                <tbody>
+                                {this.state.TopicsList.length == 0 ? 
+                                 <tr><td colSpan={5} style={{textAlign:'center'}}>No Topics Available this chapter</td></tr> : 
+                                 ''} 
+                                {this.state.TopicsList.map((data,index)=>(
+                                           
+                                           <tr key={index}>
+                                               { index == 0 ?
+                                                 <Helmet>
+                                             
+                                       <script src="assets/libs/datatables/jquery.dataTables.min.js"></script>
+                                       <script src="/assets/js/pages/datatables.init.js"></script>
+                                       <script src="assets/libs/datatables/dataTables.bootstrap4.min.js"></script>
+                                  
+                                       </Helmet> : ''}
+                                           <td>{data.fld_title}</td>
+                                           <td>{data.fld_orderno}</td>
+                                           <td style={{color:data.fld_status == 'Active' ? 'green' : 'red'}}><b>{data.fld_status}</b></td>
+                                           <td>{moment(data.fld_updatedon).format('ll')}</td>
+                                           <td className="tableact"
+                                          
+                                           ><Trash2
+                                           onClick={()=>{
+                                            confirmAlert({
+                                              title: 'Confirm to Delete',
+                                              message: 'Are you sure you want to delete topic.',
+                                              buttons: [
+                                                {
+                                                  label: 'Yes',
+                                                  onClick: () => {
+                                                      Notiflix.Loading.Dots('');
+                                  
+                                    
+                                  
+                                          PostApiCall.postRequest({
+                                        
+                                            topicid : data.fld_id,
+                                           
+                                        
+                                            },"DeleteTopic").then((results) => 
+                                            
+                                              results.json().then(obj => {
+                                  
+                                              if(results.status == 200 || results.status==201){
+                                  
+                                                  Notiflix.Loading.Remove()
+                                                  Notiflix.Notify.Success('topic successfully deleted.')
+                                                  window.location.reload()
+                                              }else
+                                              {
+                                                  Notiflix.Loading.Remove()
+                                                  Notiflix.Notify.Failure('Something went wrong, try again later.')
+                                              }
+                                          }))
+                                                  }
+                                                },
+                                                {
+                                                  label: 'No',
+                                                }
+                                              ]
+                                            });
+                                           }}
+                                           />
+                                           <span>
+                                             <Edit3 style={{marginLeft: '10px'}}
+                                             onClick={()=>{
+                                               this.setState({
+                                                 open : true,
+                                                 topicEditData : data
+                                               })
+
+                                            //    this.props.setSubCategoryName(data.fld_subcategory)
+                                            //    this.props.setSubOrder(data.fld_order)
+                                          
+                                             }}
+                                             
+                                             ></Edit3>
+                                           </span>
+                                           </td>
+                                         
+                                           </tr>
+                           
+                                     
+                                   ))}
+                                    </tbody>
+                                    </table>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                 </div>
+            </div>
+         </div>  
+
+           </div>
         )
     }
 }
-export default EduTopics;
+
+const mapStateToProps = state => ({
+    ArticleCredential : state.ArticleSubCategory
+  })
+
+
+  export default connect(mapStateToProps) (EduTopics);
